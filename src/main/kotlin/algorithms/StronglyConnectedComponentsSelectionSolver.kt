d40b8ef03a53a078714146c0d20dd8c159ddbc73
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-05-05 11:14:27 +0300

    feat: algorithm of strongly connected components selection completion
    Signed-off-by: Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>

--- src/main/kotlin/algorithms/StronglyConnectedComponentsSelectionSolver.kt
+++ src/main/kotlin/algorithms/StronglyConnectedComponentsSelectionSolver.kt
@@ -2,14 +2,15 @@ package algorithms
 
 import graphs.graphs.DirectedGraph
 
-class StronglyConnectedComponentsSelectionSolver<V> {
+class StronglyConnectedComponentsSelectionSolver<V>(val graph: DirectedGraph<V>) {
     private val visited: MutableMap<Int, Boolean> = mutableMapOf()
+    private val graphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
+    private val reversedGraphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
+    private val order: MutableList<Int> = mutableListOf()
+    private val components: MutableMap<Int, Int> = mutableMapOf()
+    private var lastComponentNum = 0
 
-    private fun initializeGraphs(
-        graph: DirectedGraph<V>,
-        graphMap: MutableMap<Int, MutableSet<Int>>,
-        reversedGraphMap: MutableMap<Int, MutableSet<Int>>
-    ) {
+    private fun initializeGraphs() {
         for (vertexNum in graph.vertices.keys) {
             for (edge in graph.edges.values) {
                 if (edge.verticesNumbers.first == vertexNum) {
@@ -24,15 +25,43 @@ class StronglyConnectedComponentsSelectionSolver<V> {
         }
     }
 
-    private fun initializeVisited(graph: DirectedGraph<V>) {
+    private fun initializeVisited() {
         for (vertexNum in graph.vertices.keys) visited[vertexNum] = false
     }
 
-    fun selectStronglyConnectedComponents(graph: DirectedGraph<V>) {
-        val graphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
-        val reversedGraphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
-        initializeGraphs(graph, graphMap, reversedGraphMap)
-        initializeVisited(graph)
-        
+    private fun dfsOrder(vertexNum: Int) {
+        visited[vertexNum] = true
+        val nextVertices = graphMap[vertexNum]
+        if (nextVertices != null) {
+            for (nextVertexNum in nextVertices) {
+                if (visited[nextVertexNum] == false) dfsOrder(nextVertexNum)
+            }
+        }
+        order.add(vertexNum)
+    }
+
+    private fun dfsComponents(vertexNum: Int) {
+        visited[vertexNum] = true
+        components[vertexNum] = lastComponentNum++
+        val nextVertices = reversedGraphMap[vertexNum]
+        if (nextVertices != null) {
+            for (nextVertexNum in nextVertices) {
+                if (visited[nextVertexNum] == false) dfsComponents(nextVertexNum)
+            }
+        }
+    }
+
+    fun selectStronglyConnectedComponents(): MutableMap<Int, Int> {
+        initializeGraphs()
+        initializeVisited()
+        for (vertexNum in graphMap.keys) {
+            if (visited[vertexNum] == false) dfsOrder(vertexNum)
+        }
+        initializeVisited() // we need visited[vertexNum] = false again for all vertices
+        for (vertexIndex in 0 until graphMap.keys.size) {
+            val vertexNum = order[graphMap.keys.size - 1 - vertexIndex]
+            if (visited[vertexNum] == false) dfsComponents(vertexNum)
+        }
+        return components
     }
 }
