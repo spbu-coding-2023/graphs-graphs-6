91d9d5e927746881ba4c056fbad5243c32a53fba
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-05-05 10:29:29 +0300

    fix and feat: toMap to InitializeGraphs changing, visited field and its initializer addition
    Signed-off-by: Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>

--- src/main/kotlin/algorithms/StronglyConnectedComponentsSelectionSolver.kt
+++ src/main/kotlin/algorithms/StronglyConnectedComponentsSelectionSolver.kt
@@ -3,19 +3,36 @@ package algorithms
 import graphs.graphs.DirectedGraph
 
 class StronglyConnectedComponentsSelectionSolver<V> {
-    private fun toMap(graph: DirectedGraph<V>) {
-        val graphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
+    private val visited: MutableMap<Int, Boolean> = mutableMapOf()
+
+    private fun initializeGraphs(
+        graph: DirectedGraph<V>,
+        graphMap: MutableMap<Int, MutableSet<Int>>,
+        reversedGraphMap: MutableMap<Int, MutableSet<Int>>
+    ) {
         for (vertexNum in graph.vertices.keys) {
             for (edge in graph.edges.values) {
                 if (edge.verticesNumbers.first == vertexNum) {
                     if (graphMap.containsKey(vertexNum)) graphMap[vertexNum]?.add(edge.verticesNumbers.second)
                     else graphMap[vertexNum] = mutableSetOf(edge.verticesNumbers.second)
+                    if (reversedGraphMap.containsKey(edge.verticesNumbers.second)) reversedGraphMap[edge.verticesNumbers.second]?.add(
+                        vertexNum
+                    )
+                    else reversedGraphMap[edge.verticesNumbers.second] = mutableSetOf(vertexNum)
                 }
             }
         }
     }
 
+    private fun initializeVisited(graph: DirectedGraph<V>) {
+        for (vertexNum in graph.vertices.keys) visited[vertexNum] = false
+    }
+
     fun selectStronglyConnectedComponents(graph: DirectedGraph<V>) {
-        val graphMap = toMap(graph)
+        val graphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
+        val reversedGraphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
+        initializeGraphs(graph, graphMap, reversedGraphMap)
+        initializeVisited(graph)
+        
     }
 }
