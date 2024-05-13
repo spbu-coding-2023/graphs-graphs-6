0d2bf81e0887fd80c822ee3c0fdde3574d139aad
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-05-13 08:39:24 +0300

    feat: stochastic matrix type change
    Signed-off-by: Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>

--- src/main/kotlin/algorithms/KeyVerticesSelectionSolver.kt
+++ src/main/kotlin/algorithms/KeyVerticesSelectionSolver.kt
@@ -5,12 +5,14 @@ import graphs.graphs.Graph
 class KeyVerticesSelectionSolver<V>(val graph: Graph<V>) {
     private val graphMap: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
     private val randomTransitionProbability = 0.15 / graph.vertices.size
-    private val stochasticMatrix = Array(graph.vertices.keys.size) { DoubleArray(graph.vertices.keys.size) { 0.0 } }
+    private val stochasticMatrix: MutableMap<Int, MutableMap<Int, Double>> = mutableMapOf()
 
     private fun initializeGraphMap() {
         for (vertexNum in graph.vertices.keys) {
             for (edge in graph.edges.values) {
                 if ((edge.verticesNumbers.first == vertexNum) || (edge.verticesNumbers.second == vertexNum)) {
+                    if (graphMap.containsKey(vertexNum)) graphMap[vertexNum]?.add(edge.verticesNumbers.second)
+                    else graphMap[vertexNum] = mutableSetOf(edge.verticesNumbers.second)
                     if (graphMap.containsKey(vertexNum)) graphMap[vertexNum]?.add(edge.verticesNumbers.second)
                     else graphMap[vertexNum] = mutableSetOf(edge.verticesNumbers.second)
                 }
@@ -19,6 +21,7 @@ class KeyVerticesSelectionSolver<V>(val graph: Graph<V>) {
     }
 
     private fun initializeStochasticMatrix() {
+
     }
 
     fun selectKeyVertices() {
