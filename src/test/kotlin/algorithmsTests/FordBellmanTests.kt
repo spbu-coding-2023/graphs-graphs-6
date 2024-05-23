ecf423cb3d3a19e7624fd5e2d35bc35669ec1458
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-05-23 11:20:21 +0300

    chores: renaming of tests for Ford-Bellman algorithm
    Signed-off-by: Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>

--- src/test/kotlin/algorithmsTests/FordBellmanTests.kt
+++ src/test/kotlin/algorithmsTests/FordBellmanTests.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-class PathSearchWithNegativeWeightsSolverTests {
+class FordBellmanTests {
     private lateinit var graph: WeightedDirectedGraph<Int>
     private lateinit var solver: FordBellmanAlgorithm<Int>
 
