8c587e153282df06250e1bff2692457c006d714c
Dmitri Kuznetsov <dmitrvlkuznetsov@gmail.com>
2024-05-26 18:37:15 +0300

    fix: removing gui parts from wrong branch

--- src/main/kotlin/viewModel/graphViewModel/RepresentationStrategy.kt
+++
@@ -1,6 +0,0 @@
-package viewModel.graphViewModel
-
-interface RepresentationStrategy {
-    fun <V> place(width: Double, height: Double, vertices: Collection<VertexViewModel<V>>)
-    fun <V> highlight(vertices: Collection<VertexViewModel<V>>)
-}
