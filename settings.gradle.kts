ec330043bd2d278f82bf8ef4e5b2a9d7737383b8
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-05-26 18:53:43 +0300

    fix: making gradle files similiar to development branch
    Signed-off-by: Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,4 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-    plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-        id("org.jetbrains.compose").version(extra["compose.version"] as String)
-    }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "graphs-graphs-6"
