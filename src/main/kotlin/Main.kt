7ea9b4622daab3c48e37c9fd3b708d08294228b0
Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>
2024-05-26 17:39:48 +0300

    feat: addition of graph choose window
    Signed-off-by: Vyacheslav Kochergin <vyacheslav.kochergin1@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import model.graphs.graphs.Graph
+import view.graphChooseScreen
 import view.mainScreen
+import viewModel.GraphChooseViewModel
 import viewModel.MainScreenViewModel
 import viewModel.graphViewModel.CircularPlacementStrategy
 
@@ -25,11 +30,22 @@ val sampleGraph = Graph<String>().apply {
     addEdge(0, 6)
 }
 
+var closeFirstWindow: MutableState<Boolean> = mutableStateOf(false)
+
 @Composable
 @Preview
 fun app() {
     MaterialTheme {
-        mainScreen(MainScreenViewModel(sampleGraph, CircularPlacementStrategy()))
+        closeFirstWindow = remember { mutableStateOf(false) }
+        if (!closeFirstWindow.value) {
+            graphChooseScreen(GraphChooseViewModel<String>()) {
+                closeFirstWindow.value = true
+            }
+        }
+
+        if (closeFirstWindow.value) {
+            mainScreen(MainScreenViewModel(sampleGraph, CircularPlacementStrategy()))
+        }
     }
 }
 
